ca3c60116c4f207ee20e89f29aef55fef096c195
evidhya <82639166+evidhya@users.noreply.github.com>
2024-01-19 10:29:18 +0530

    Update s3-state-locking.tf
    added bucket name

--- Section 5 - Remote state management/terraform - multistate/s3-state-locking.tf
+++ Section 5 - Remote state management/terraform - multistate/s3-state-locking.tf
@@ -3,7 +3,7 @@
 
 terraform {
   backend "s3" {
-    bucket = "kplabs-terraform-backend"
+    bucket = "my124-terraform-backend"
     key    = "network/demo.tfstate"
     region = "us-east-1"
     dynamodb_table = "terraform-state-locking"
